e5a84e6252590fb7f10e467261dffb4942423a3f
Badams <badams10@gmail.com>
2019-10-09 23:23:05 -0500

    html routes server

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+        name: "Mike",
+        image: "https://randomuser.me/api/portraits/men/68.jpg",
+        scores: [2, 1, 5, 4, 3]
+    },
+    {
+        name: "Eliza",
+        image: "https://randomuser.me/api/portraits/women/24.jpg",
+        scores: [5, 1, 8, 1, 2]
+    },
+    {
+        name: "Jillian",
+        image: "https://randomuser.me/api/portraits/women/2.jpg",
+        scores: [5, 7, 1, 2, 5, 3]
+    }
+];
+
+module.exports = friends;
